3ca03b163f69e8f53910a3501ba2f10d7f841efe
ValderramaLlajarunaEnrique <evalderramal1@upao.edu.pe>
2026-09-10 10:09:54 -0500

    feat: implementar la API mínima

---
+++ index.js
@@ -0,0 +1,19 @@
+﻿const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    laboratorio: 'Laboratorio 02 - Docker Compose',
+    mensaje: 'Hola desde la API! Estudiante: Valderrama Llajaruna Enrique',
+    puerto: PORT,
+    timestamp: new Date().toISOString()
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(Servidor iniciado en el puerto  - Valderrama Llajaruna Enrique);
+});
